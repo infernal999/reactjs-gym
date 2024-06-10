ed847fa611b8683dd447ac957ee9087859dd2d6e
Ferencyk Mikhailo <86631675+infernal999@users.noreply.github.com>
2024-06-11 00:19:54 +0200

    updates to gym app

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
 
 export default function Generator() {
   return (
-    <div>Generator</div>
+    <SectionWrapper header={'Generate your workout'} title={['It\'s', 'Huge', 'o\'clock']}>
+
+{/* 2:11:00 */}
+    </SectionWrapper>
   )
 }
